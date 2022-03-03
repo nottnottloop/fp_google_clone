91fc120591ce2e05764ff6d1335c0b5601b36447
nottnottloop <6772127+nottnottloop@users.noreply.github.com>
2022-03-03 11:41:08 +0000

    serve files on /

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const cors = require('cors');
+const path = require('path');
 
 const port = 3000;
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, "../client")));
 
 const routes = require("./controller");
 app.use('/', routes);
